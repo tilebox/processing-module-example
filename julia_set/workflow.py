01f655f7b0ad5b79510fecb70c79a96a86035477
Stefan Amberger <1277330+snamber@users.noreply.github.com>
2022-09-12 12:18:34 +0200

    Improve code formatting (#3)

--- julia_set/workflow.py
+++ julia_set/workflow.py
@@ -4,7 +4,7 @@ import sys
 from tilebox_processing.cache import GoogleStorageCache, LocalCache
 from tilebox_processing.tasks import current_task_id, new_task
 
-from julia_set.main import concatenate_rows, julia, save_figure
+from julia_set.main import concatenate_rows, calculate_julia, save_figure
 
 OUTPUT_CLUSTER = os.getenv("JULIA_OUTPUT_CLUSTER", "default")
 COMPUTE_CLUSTER = os.getenv("JULIA_COMPUTE_CLUSTER", "default")
@@ -13,6 +13,27 @@ if os.getenv("TILEBOX_CACHE_DIRECTORY"):
     cache = LocalCache()
 else:
     cache = GoogleStorageCache()
+
+
+def calculate_julia_task():
+    """ calculate starts """
+    print("started calculation of julia sub-set")
+    size, start, end, name = int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), sys.argv[4]
+
+    if size * (end - start) < 500_000:
+        arr = calculate_julia(size, start, end)
+        cache.save(current_task_id(), arr, "output")
+        if size == end - start:
+            new_task("save-figure", current_task_id(), size, name, cluster=OUTPUT_CLUSTER)
+    else:
+        mid = int(start + (end - start) / 2)
+        task1 = new_task("calculate-julia", size, start, mid, name, cluster=COMPUTE_CLUSTER)
+        task2 = new_task("calculate-julia", size, mid, end, name, cluster=COMPUTE_CLUSTER)
+        task3 = new_task("combine-outputs", current_task_id(), task1, task2, dependencies=[task1, task2],
+                         cluster=COMPUTE_CLUSTER)
+        if size == end - start:  # final task
+            print("saving figure")
+            new_task("save-figure", current_task_id(), size, name, dependencies=[task3], cluster=OUTPUT_CLUSTER)
 
 
 def combine_outputs_task():
@@ -33,24 +54,3 @@ def save_figure_task():
     arr = cache.load(task, "output")
 
     save_figure(name, imsize, imsize, imsize, arr)
-
-
-def calculate_julia_task():
-    """ calculate starts """
-    print("started calculation of julia sub-set")
-    size, start, end, name = int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), sys.argv[4]
-
-    if size * (end - start) < 500_000:
-        arr = julia(size, start, end)
-        cache.save(current_task_id(), arr, "output")
-        if size == end - start:
-            new_task("save-figure", current_task_id(), size, name, cluster=OUTPUT_CLUSTER)
-    else:
-        mid = int(start + (end - start) / 2)
-        task1 = new_task("calculate-julia", size, start, mid, name, cluster=COMPUTE_CLUSTER)
-        task2 = new_task("calculate-julia", size, mid, end, name, cluster=COMPUTE_CLUSTER)
-        task3 = new_task("combine-outputs", current_task_id(), task1, task2, dependencies=[task1, task2],
-                         cluster=COMPUTE_CLUSTER)
-        if size == end - start:  # final task
-            print("saving figure")
-            new_task("save-figure", current_task_id(), size, name, dependencies=[task3], cluster=OUTPUT_CLUSTER)
